431f9a48e12b90258f9041fbbed241d68e425e81
Francesco2426 <77923481+Francesco2426@users.noreply.github.com>
2022-09-10 12:39:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-import random, string, os, time, requests, sys
+import random, string, os, time, requests
 from colorama import Fore
 
 # Colour Variables
 lb = Fore.LIGHTBLUE_EX
-y = Fore.LIGHTYELLOW_EX
+ly = Fore.LIGHTYELLOW_EX
+y = Fore.YELLOW
 r = Fore.RESET
 cy = Fore.LIGHTCYAN_EX
 lg = Fore.LIGHTGREEN_EX
 lw = Fore.LIGHTWHITE_EX
 g = Fore.GREEN
 
-def cls():
-    os.system('cls')
-
 # Letter and number string generated
-def letter_num(length: int):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+def letter_num():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(12, 24)))
 
 # Letter, number and special character string generated
-def letter_num_char(length: int):
-    return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+def letter_num_char():
+    return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(12, 24)))
 
-# Phrase string gen
-def phrase():
-    r = requests.get('https://random-word-api.herokuapp.com/word?number=1&swear=0')
-    return r.json()[0]
+def phrase_num_char():
+    password = ''
+    for char in requests.get('https://random-word-api.herokuapp.com/word?number=1&length=6').json()[0]:
+        password += char + str(random.randint(10, 99)) + ''.join(random.choices(string.punctuation, k=random.randint(3, 5)))
+    return password
 
 # Amount of passwords needed to be generated
 def amount_pass():
@@ -35,55 +34,45 @@ def amount_pass():
         print('That\'s not a number.')
         time.sleep(2.5)
         menu()
-        
+
 # Main menu
 def menu():
-    cls()
+    os.system('cls')
     print(f'''
-    {lb} _____         _____ _____  {y}  _____ ______ _   _ 
-    {lb}|  __ \ /\    / ____/ ____| {y} / ____|  ____| \ | |
-    {lb}| |__) /  \  | (___| (___   {y}| |  __| |__  |  \| |
-    {lb}|  ___/ /\ \  \___ \\\___ \ {y} | | |_ |  __| | . ` |
-    {lb}| |  / ____ \ ____) |___) | {y}| |__| | |____| |\  |
-    {lb}|_| /_/    \_\_____/_____/  {y} \_____|______|_| \_|                                             
+    {lb} _____         _____ _____  {ly}  _____ ______ _   _ 
+    {lb}|  __ \ /\    / ____/ ____| {ly} / ____|  ____| \ | |
+    {lb}| |__) /  \  | (___| (___   {ly}| |  __| |__  |  \| |
+    {lb}|  ___/ /\ \  \___ \\\___ \ {ly} | | |_ |  __| | . ` |
+    {lb}| |  / ____ \ ____) |___) | {ly}| |__| | |____| |\  |
+    {lb}|_| /_/    \_\_____/_____/  {ly} \_____|______|_| \_|                                             
     {r}
     Choose password type:
 
     [{cy}1{r}] Phrase + Numbers & Special Characters - {lg}Most Secure{r}                                           
-    [{cy}2{r}] Letters + Numbers & Special Characters - {lg}Extremely Secure{r}
-    [{cy}3{r}] Letters + Numbers - {g}Very Secure{r}
-    [{cy}4{r}] Exit{r}
+    [{cy}2{r}] Letters + Numbers & Special Characters - {g}Extremely Secure{r}
+    [{cy}3{r}] Letters + Numbers - {y}Secure{r}
     '''.center(os.get_terminal_size().columns))
+
     try:
         choice = int(input('> '))
-    except ValueError:
-        print('That\'s not a number.')
-        time.sleep(2.5)
+        
+        if not choice in [1, 2, 3]:
+            print('Not a valid option')
+            time.sleep(2)
+            menu()
+            
+        for i in range(amount_pass()):
+            options = {
+                1: phrase_num_char(),
+                2: letter_num_char(),
+                3: letter_num()
+            }
+            password = options[choice]
+            print(f'[{y}{i + 1}{r}] {password}')
+        time.sleep(1)
+        input(F'\n{lw}Press enter to return to the main menu.{r}')
         menu()
-    if choice == 1:
-        amount = amount_pass()
-        for i in range(amount):
-            print(phrase() + str(random.randint(10, 255)) + ''.join(random.choices(string.punctuation, k=random.randint(2, 4))))
-        time.sleep(2.5)
-        input(F'\n{lw}Press any key to return to the main menu.{r}')
-        menu()
-    elif choice == 2:
-        amount = amount_pass()
-        for i in range(amount):
-            print(letter_num_char(random.randint(12, 24)))
-        time.sleep(2.5)
-        input(F'\n{lw}Press any key to return to the main menu.{r}')
-        menu()
-    elif choice == 3:
-        amount = amount_pass()
-        for i in range(amount):
-            print(letter_num(random.randint(12, 24)))
-        time.sleep(2.5)
-        input(F'\n{lw}Press any key to return to the main menu.{r}')
-        menu()
-    elif choice == 4:
-        sys.exit(0)
-    else:
+    except:
         print('Not a valid option')
         time.sleep(2)
         menu()
