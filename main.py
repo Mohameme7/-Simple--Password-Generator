09af967a1bf0a2e77cb5d1eda164a7867696df30
Mohameme7 <101041823+Mohameme7@users.noreply.github.com>
2022-09-20 23:18:01 +0400

    Update main.py
    Yes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random, string, os, time, requests
+import random, string, os, time, aiohttp, asyncio
 from colorama import Fore
-
 # Colour Variables
+
 lb = Fore.LIGHTBLUE_EX
 ly = Fore.LIGHTYELLOW_EX
 y = Fore.YELLOW
@@ -10,33 +10,44 @@ cy = Fore.LIGHTCYAN_EX
 lg = Fore.LIGHTGREEN_EX
 lw = Fore.LIGHTWHITE_EX
 g = Fore.GREEN
-
 # Letter and number string generated
-def letter_num():
+async def letter_num():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(12, 24)))
 
 # Letter, number and special character string generated
-def letter_num_char():
+async def letter_num_char():
     return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(12, 24)))
 
-def phrase_num_char():
+# Number, phrase, punctuation string generated
+async def phrase_num_char():
     password = ''
-    for char in requests.get('https://random-word-api.herokuapp.com/word?number=1&length=6').json()[0]:
-        password += char + str(random.randint(10, 99)) + ''.join(random.choices(string.punctuation, k=random.randint(3, 5)))
+    async with aiohttp.ClientSession() as session:
+
+         request = await session.get('https://random-word-api.herokuapp.com/word?number=1&length=6')
+         json = await request.json()
+         id = json[0]
+         for char in id:
+          password += char + str(random.randint(10, 99)) + ''.join(
+          random.choices(string.punctuation, k=random.randint(3, 5)))
     return password
 
 # Amount of passwords needed to be generated
-def amount_pass():
+
+async def amount_pass():
     try:
         option = int(input('How many passwords do you want to generate? > '))
+
         return option
     except:
         print('That\'s not a number.')
         time.sleep(2.5)
-        menu()
+        await menu()
+
 
 # Main menu
-def menu():
+
+async def menu():
+
     os.system('cls')
     print(f'''
     {lb} _____         _____ _____  {ly}  _____ ______ _   _ 
@@ -47,7 +58,6 @@ def menu():
     {lb}|_| /_/    \_\_____/_____/  {ly} \_____|______|_| \_|                                             
     {r}
     Choose password type:
-
     [{cy}1{r}] Phrase + Numbers & Special Characters - {lg}Most Secure{r}                                           
     [{cy}2{r}] Letters + Numbers & Special Characters - {g}Extremely Secure{r}
     [{cy}3{r}] Letters + Numbers - {y}Secure{r}
@@ -55,25 +65,40 @@ def menu():
 
     try:
         choice = int(input('> '))
-        
+
         if not choice in [1, 2, 3]:
             print('Not a valid option')
-            time.sleep(2)
-            menu()
-            
-        for i in range(amount_pass()):
-            options = {
-                1: phrase_num_char(),
-                2: letter_num_char(),
-                3: letter_num()
-            }
-            password = options[choice]
-            print(f'[{y}{i + 1}{r}] {password}')
-        time.sleep(1)
+            await asyncio.sleep(2)
+            await menu()
+        tasks = []
+        for i in range(await amount_pass()):
+           options = {
+               1:  phrase_num_char,
+               2:  letter_num_char,
+               3:  letter_num
+           }
+           task = asyncio.create_task(options[choice]())
+           tasks.append( task)
+        async def gather():
+            passwords = await asyncio.gather(*tasks)
+            return passwords
+        passwords = await gather()
+        numofpasses = 0
+        for e in passwords:
+            numofpasses += 1
+            print(f'[{y}{numofpasses}{r}] {e}')
+
+
+
+        await asyncio.sleep(1)
+
         input(F'\n{lw}Press enter to return to the main menu.{r}')
-        menu()
-    except:
+        await menu()
+    except Exception as e:
         print('Not a valid option')
+
         time.sleep(2)
-        menu()
-menu()
+        await menu()
+if __name__ == "__main__":
+    asyncio.run(menu())
+
